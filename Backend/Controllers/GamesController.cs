21b2fbc9dfa097f440bac07b75e799763701b916
Jeremy <bishopjc@etsu.edu>
2023-08-16 13:09:49 -0400

    Create an API controller for games with an example action method.

---
+++ Backend/Controllers/GamesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    /// <summary>
+    /// Defines routes and handles actions related to games.
+    /// </summary>
+    [Route("[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult Example()
+        {
+            var testObject = new
+            {
+                Id = 8,
+                Text = "Hello"
+            };
+            return Ok(testObject);
+        }
+    }
+}
