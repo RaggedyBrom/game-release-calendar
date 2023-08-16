7fa100053af6797aef33562480a9bd19989e908e
Jeremy <bishopjc@etsu.edu>
2023-08-16 16:46:30 -0400

    Add basic support for CORS in backend.

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,12 +4,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 #region Builder setup
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.WithOrigins("*");
+        });
+    });
 #endregion
 
 var app = builder.Build();
 
 #region Middleware
 app.UseHttpsRedirection();
+app.UseCors();
 app.MapControllers();
 #endregion
 
